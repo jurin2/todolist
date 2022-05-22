70bd84034e8e34047119b88ec1f1cb6e4b8863f5
kiyo0811 <kiyo0811@naver.com>
2022-05-22 17:52:08 +0900

    투두리스트 작업중2

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 // + 버튼을 클릭하면, 할일이 추가된다
 // delete버튼을 누르면 할일이 삭제된다
 // check버튼을 누르면 할일이 끝나면서 밑줄이 간다
+// 1. check버튼을 클릭하는 순간 true false
+// 2. true이면 끝난걸로 간주하고 밑줄 보여주기
+// 3. false이면 안끝난걸로 간주하고 그대로
 // 진행중 완료탭을 누르면, 언더바가 이동한다
 // 완료탭은 완료 아이템만 진행중탭은 진행중인 아이템만
 // 전체탭을 누르면 다시 전체아이템으로 돌아온다
@@ -13,8 +16,12 @@ let taskList = [];
 addButton.addEventListener("click", addTask);
 
 function addTask(){
-    let taskContent = taskInput.value;
-    taskList.push(taskContent);
+    let task = {
+        id:randomIDGenerate(),
+        taskContent : taskInput.value,
+        isComplete : false
+    }
+    taskList.push(task);
     console.log(taskList);
     render();
 }
@@ -22,15 +29,38 @@ function addTask(){
 function render(){
     let resultHTML = '';
     for(let i=0; i<taskList.length; i++){
-        resultHTML += `<div class="task">
-        <div>
-            ${taskList[i]}
-        </div>
-        <div>
-            <button>Check</button>
-            <button>Delete</button>
-        </div>
-    </div>`
+        if(taskList[i].isComplete == true){
+            resultHTML +=` <div class="task">
+                                <div class="task-done">${taskList[i].taskContent}</div>
+                                <div>
+                                    <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+                                    <button>Delete</button>
+                                </div>
+                            </div>`
+        }else{
+            resultHTML += ` <div class="task">
+                                <div>${taskList[i].taskContent}</div>
+                                <div>
+                                    <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+                                    <button>Delete</button>
+                                </div>
+                            </div>`
+        }
     }
     document.getElementById("task-board").innerHTML = resultHTML;
+}
+
+function toggleComplete(id){
+    for(let i=0; i<taskList.length; i++){
+       if(taskList[i].id == id){
+        taskList[i].isComplete  = true
+        break;
+       }
+    }
+    render();
+    console.log(taskList);
+}
+
+function randomIDGenerate(){
+    return '_' + Math.random().toString(36).substring(2, 9)
 }
